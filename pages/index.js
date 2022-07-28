fdca9bd383fe3f4851c1e0f3e2a2781d1be173b4
weartstudio <balazs@weart.hu>
2022-07-28 18:14:04 +0200

    index

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
+
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
