f52aa15ee0d3d6033dd3901229f93405f20b3ebe
weartstudio <balazs@weart.hu>
2022-08-16 14:59:45 +0200

    menu navigation

--- components/Menu.js
+++ components/Menu.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Menu() {
 	return (
 		<nav>
-			<a href="/" className="logo">weart</a>
+			<Link href="/">
+				<a className='logo'>weart</a>
+			</Link>
 			<ol>
-				<li><a href="#rolam">Rólam</a></li>
-				<li><a href="#portfolio">Portfólió</a></li>
-				<li><a href="#blog">Blog</a></li>
-				<li><a href="#kapcsolat">Kapcsolat</a></li>
+				<li><Link href="/rolam">Rólam</Link></li>
+				<li><Link href="/portfolio">Portfólió</Link></li>
+				<li><Link href="/blog">Blog</Link></li>
+				<li><Link href="/kapcsolat">Kapcsolat</Link></li>
 			</ol>
 		</nav>
 	)
