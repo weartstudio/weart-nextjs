244952c96e507d4defa5c6b8e86e8893146ac7d1
weartstudio <balazs@weart.hu>
2022-09-21 12:39:18 +0200

    small changes

--- components/Hero.js
+++ components/Hero.js
@@ -5,9 +5,9 @@ function Hero({ data }) {
 	return (
 		<Row id="hero" className='align-items-center text-center text-lg-start pt-4'>
 			<Col lg={6} className="py-5 text-white">
-				<h1 className='h2'>Sikeres Front-end Fejlesztő</h1>
-				<p className='lead'>Helló, Egyed Balázs vagyok egy szabadúszó front-end fejlesztő több mint 8 éves tapasztalattal. Weboldalaimat főleg React, és MVC alapú WordPress megoldásokkal készítem el.</p>
-				<Button href="https://myfolio-portfolio-webflow-template.webflow.io/">Portfolió</Button>
+				<h1 className='h2'>Helló, egy Front-end fejlesztő vagyok</h1>
+				<p className='lead'>Egyed Balázs vagyok egy szabadúszó front-end fejlesztő több mint 8 éves tapasztalattal. Weboldalaimat főleg React, és MVC alapú WordPress megoldásokkal készítem el, de sok egyéb technológiát is ismerek.</p>
+				<Button size="lg" href="https://myfolio-portfolio-webflow-template.webflow.io/">Portfolió</Button>
 			</Col>
 			<Col className='text-end'>
 				<div className="hero-img">
