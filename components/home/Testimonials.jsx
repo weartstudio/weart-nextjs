9a2d0fb762fa3785c7f33092922779780f3da83f
weartstudio <balazs@weart.hu>
2023-01-19 15:04:05 +0100

    testimonial

--- components/home/Testimonials.jsx
+++ components/home/Testimonials.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react'
 import { Container, Carousel, Row, Col, Button } from 'react-bootstrap'
-import TestimonialItem from './TestimonialItem'
-import { testimonialQuery } from '../../helpers/queries'
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 
 function Testimonials({data}) {
 
 	return (
-		<div className='py-lg-8 py-5  bg-dark text-white'>
+		// <div className='py-lg-8 py-5  bg-dark text-white'>
 			<Container style={{minHeight: 250}}>
 				<Row className='justify-content-center'>
 					<Col lg='9'>
 						<Carousel slide={true} controls={false} indicators={false}>
 							{data.testimonialblockcontent.map((item,i)=>(
 								<Carousel.Item key={i}>
-									<TestimonialItem data={item} />
+									<blockquote className='mb-0 d-flex flex-column'>
+										<p className=''>{item?.text}</p>
+										<cite className='ms-auto'><span className='text-primary'>{item?.name}<br/>{item?.tag}</span></cite>
+									</blockquote>
 								</Carousel.Item>
 							))}
 						</Carousel>
 					</Col>
 				</Row>
 			</Container>
-		</div>
+		// </div>
 	)
 }
 
