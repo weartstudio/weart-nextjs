d49e0ab3e8b0abb2600d4dbbfad0ce7525570040
weartstudio <balazs@weart.hu>
2023-03-02 15:14:12 +0100

    title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Weart.hu - Egyed Balázs, JS, React és WordPress fejlesztő</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Menu />
